cc8bf6045977c4cd9de6599235c62f8668683e5e
Sotiris Thomas <sotirios.thomas@jsrmicro.be>
2025-03-12 09:56:00 +0100

    added WaferMapGrid class to the module

--- wafermap/__init__.py
+++ wafermap/__init__.py
@@ -2,4 +2,4 @@
 
 # from wafermap(package).wafermap(module) we import class WaferMap into the package
 # namespace.
-from wafermap.wafermap import WaferMap
+from wafermap.wafermap import WaferMap, WaferMapGrid
